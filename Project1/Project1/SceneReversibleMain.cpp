b26d8025d367d1f8c5589d4748f70532604b4fab
友渕　泰清 <dj.yatsh@gmail.com>
2019-10-24 12:32:14 +0900

    白背景挿入

--- Project1/Project1/SceneReversibleMain.cpp
+++ Project1/Project1/SceneReversibleMain.cpp
@@ -33,6 +33,7 @@ void CSceneReversibleMain::InitScene()
 	//グラフィック読み込み
 	Draw::LoadImage(L"パズルパネル黒5×5用.png", 0, TEX_SIZE_512);
 	Draw::LoadImage(L"パネル白(96x96).png", 1, TEX_SIZE_512);
+	Draw::LoadImage(L"白背景.png", 2, TEX_SIZE_512);
 
 
 	CObjReversibleMain* p = new CObjReversibleMain();
