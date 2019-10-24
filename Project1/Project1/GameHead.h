14f71c0f63a20aed1f8cac0a2c420733baa7546d
友渕　泰清 <dj.yatsh@gmail.com>
2019-10-24 11:48:07 +0900

    マウスのシーン表示で干渉？

--- Project1/Project1/GameHead.h
+++ Project1/Project1/GameHead.h
@@ -65,7 +65,7 @@ struct UserData
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START CSceneTitle  
+#define SET_GAME_START CSceneReversibleMain
 //-----------------------------------------------
 
 //テストだよ～～
