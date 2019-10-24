8626c7cae7ae0caa097c8c588b38247f37d1d91e
友渕　泰清 <dj.yatsh@gmail.com>
2019-10-24 14:12:25 +0900

    マウスの関数化

--- Project1/Project1/UtilityModule.cpp
+++ Project1/Project1/UtilityModule.cpp
@@ -1,3 +1,20 @@
 #include "GameL\DrawTexture.h"
 #include "GameHead.h"
 #include "UtilityModule.h"
+#include"GameL/WinInputs.h"
+
+int MousePosX()
+{
+	float m_mou_x;
+	m_mou_x = (float)Input::GetPosX();
+	return m_mou_x;
+}
+
+int MousePosY()
+//マウスの位置を取得Y
+{
+	float m_mou_y;
+	m_mou_y = (float)Input::GetPosY();
+	return m_mou_y;
+}
+
--- Project1/Project1/UtilityModule.h
+++ Project1/Project1/UtilityModule.h
@@ -1,1 +1,5 @@
 #pragma once
+
+int MousePosX();
+
+int MousePosY();
