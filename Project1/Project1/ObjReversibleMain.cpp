0c8737b5c641b2e08aa172014cbb0709d05c028e
蓮 新改 <xingailian4r@gmail.com>
2019-10-24 11:50:39 +0900

    メインの原型(？)

---
+++ Project1/Project1/ObjReversibleMain.cpp
@@ -0,0 +1,28 @@
+#include "GameL\DrawTexture.h"
+#include "GameL\WinInputs.h"
+#include "GameL\SceneManager.h"
+#include "GameL\SceneObjManager.h"
+
+#include "GameHead.h"
+#include "ObjReversibleMain.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjReversibleMain::Init()
+{
+
+}
+
+//アクション
+void CObjReversibleMain::Action()
+{
+
+}
+
+//ドロー
+void CObjReversibleMain::Draw()
+{
+
+}
--- Project1/Project1/ObjReversibleMain.h
+++ Project1/Project1/ObjReversibleMain.h
@@ -1,1 +1,20 @@
 #pragma once
+//使用するヘッダー
+#include "GameL\SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：Reversibleメイン
+class CObjReversibleMain : public CObj
+{
+public:
+	CObjReversibleMain(int map[5][5]);
+	~CObjReversibleMain() {};
+	void Init();    //イニシャライズ
+	void Action();  //アクション
+	void Draw();    //ドロー
+
+private:
+	int stage[5][5];
+
+};
