fabf76cf71ec4315e5e025ec1b6e797c727cd6dc
gravity0523 <baatximiasu523@gmail.com>
2019-12-20 10:27:24 +0900

    スイッチ画像差し替え ギャラリーのマクロ化

--- Project1/Project1/SceneGallery.cpp
+++ Project1/Project1/SceneGallery.cpp
@@ -50,7 +50,7 @@ void CSceneGallery::InitScene()
 		}
 		if (i == 2)
 		{
-			Draw::LoadImage(L"太陽2.png", 3, TEX_SIZE_4096);
+			Draw::LoadImage(L"まとめ画像/スイッチクリア.png", 3, TEX_SIZE_4096);
 		}
 	}
 	for (int i = 0; i < 6; i++)
@@ -65,7 +65,7 @@ void CSceneGallery::InitScene()
 		}
 	}
 
-	Draw::LoadImage(L"まとめ画像/黒背景.png", 6, TEX_SIZE_512);
+	Draw::LoadImage(L"まとめ画像/黒背景.png", 5, TEX_SIZE_512);
 
 	Draw::LoadImage(L"まとめ画像/マウス、シーン、クリア（透過）.png", 10, TEX_SIZE_1024);
 
