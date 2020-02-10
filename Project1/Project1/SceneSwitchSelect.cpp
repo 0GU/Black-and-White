aa0b3d627d6e23fb6b736966c90f1fbef7294551
友渕　泰清 <dj.yatsh@gmail.com>
2020-02-10 12:02:02 +0900

    BGM追加！！

--- Project1/Project1/SceneSwitchSelect.cpp
+++ Project1/Project1/SceneSwitchSelect.cpp
@@ -31,7 +31,7 @@ CSceneSwitchSelect::~CSceneSwitchSelect()
 void CSceneSwitchSelect::InitScene()
 {
 	//SE情報の登録
-	Audio::LoadAudio(0, L"Sound/タイトル.wav", SOUND_TYPE::BACK_MUSIC);
+	Audio::LoadAudio(0, L"Sound/スイッチセレクト.wav", SOUND_TYPE::BACK_MUSIC);
 	Audio::LoadAudio(1, L"Sound/SelectButton.wav", EFFECT);
 	Audio::LoadAudio(2, L"Sound/BackButton.wav", EFFECT);
 
